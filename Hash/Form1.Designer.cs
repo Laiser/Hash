30c417d3b2c530128fa6f8c0a431986288544501
Laiser <laisermello@gmail.com>
2018-08-20 18:17:10 -0300

    Upload do trabalho sobre hash.

---
+++ Hash/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridHash = new System.Windows.Forms.DataGridView();
+            this.cbHash = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(72, 86);
+            this.tbValor.MaxLength = 11;
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(121, 20);
+            this.tbValor.TabIndex = 0;
+            this.tbValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbValor_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Hash.Properties.Resources._6098_32x32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(227, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 41);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridHash
+            // 
+            this.dataGridHash.AllowUserToAddRows = false;
+            this.dataGridHash.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridHash.Location = new System.Drawing.Point(23, 134);
+            this.dataGridHash.Name = "dataGridHash";
+            this.dataGridHash.Size = new System.Drawing.Size(225, 487);
+            this.dataGridHash.TabIndex = 2;
+            // 
+            // cbHash
+            // 
+            this.cbHash.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbHash.FormattingEnabled = true;
+            this.cbHash.Items.AddRange(new object[] {
+            "",
+            "Folding",
+            "MidSquare"});
+            this.cbHash.Location = new System.Drawing.Point(72, 27);
+            this.cbHash.Name = "cbHash";
+            this.cbHash.Size = new System.Drawing.Size(121, 21);
+            this.cbHash.TabIndex = 3;
+            this.cbHash.SelectedIndexChanged += new System.EventHandler(this.cbHash_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hash:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(275, 248);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 238);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(263, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "VETOR DOS VALORES:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 644);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbHash);
+            this.Controls.Add(this.dataGridHash);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbValor);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hash";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHash)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridHash;
+        private System.Windows.Forms.ComboBox cbHash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Hash/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Hash
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridHash.ColumnCount = 2;
+            dataGridHash.RowCount = 21;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            if (tbValor.Text == "")
+            {
+                MessageBox.Show("Informe um valor!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cbHash.Text == "")
+            {
+                MessageBox.Show("Escolha um métod de hash!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+
+                dataGridHash.Columns[0].Width = 50;
+                dataGridHash.Columns[1].Width = 130;
+
+                dataGridHash.Columns[0].Name = "Posição";
+                dataGridHash.Columns[1].Name = "Matricula";
+
+                for (int i = 0; i < dataGridHash.RowCount; i++)
+                {
+                    dataGridHash.Rows[i].Cells[0].Value = i;
+                }
+            }
+
+            verificaPreenchimento();
+
+        }
+
+
+        public void metodoMidSquare()
+        {
+            string x = (Math.Pow(Convert.ToInt32(tbValor.Text), 2)).ToString();
+            int[] vetorMatricula = new int[x.Length];
+            var lista = new List<Int32>();
+            string soma = "";
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Int32 num = 0;
+                if (Int32.TryParse(x.Substring(i, 1), out num))
+                    lista.Add(num);
+                listBox1.Items.Add("posicao:" + i + "==>" + lista[i]);
+                vetorMatricula[i] = lista[i];
+
+            }
+
+
+            int meio = (vetorMatricula.Length / 2) - 1;
+
+            soma += vetorMatricula[meio].ToString() + vetorMatricula[meio + 1].ToString();
+
+            int resultado = (Convert.ToInt32(soma)) % 21;
+
+            MessageBox.Show("O valor : " + tbValor.Text + " será armazanado na posição :" + Convert.ToString(resultado) + " , ou na próxima posição vazia", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+
+            testaColisao(resultado);
+
+        }
+
+
+        public void metodoFolding()
+        {
+
+            var lista = new List<Int32>();
+            var x = tbValor.Text;
+            int[] vetorMatricula = new int[x.Length];
+
+            int soma = 0;
+            if (tbValor.Text.Length != 0)
+            {
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    Int32 num = 0;
+                    if (Int32.TryParse(x.Substring(i, 1), out num))
+                        lista.Add(num);
+                    listBox1.Items.Add("posicao:" + i + "==>" + lista[i]);
+                    vetorMatricula[i] = lista[i];
+
+                }
+
+
+            }
+
+            for (int i = 0; i <= (vetorMatricula.Length - 1);)
+            {
+                if (i == (vetorMatricula.Length - 1))
+                {
+                    soma += vetorMatricula[i];
+                    i = i + 2;
+                }
+                else
+                {
+                    string valor = vetorMatricula[i].ToString() + vetorMatricula[i + 1].ToString();
+                    soma += Convert.ToInt32(valor);
+
+                    i = i + 2;
+                }
+
+            }
+
+
+            MessageBox.Show("O valor : " + tbValor.Text + " será armazanado na posição :" + Convert.ToString(soma % x.Length) + " , ou na próxima posição vazia", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+
+            int resultado = soma % x.Length;
+
+            testaColisao(resultado);
+
+        }
+
+
+        public void verificaPreenchimento()
+        {
+            int contador = 0;
+            for (int i = 0; i < dataGridHash.RowCount; i++)
+            {
+                if (dataGridHash.Rows[i].Cells[1].Value != null)
+                {
+                    contador = contador + 1;
+
+                    if (contador == 21)
+                    {
+                        MessageBox.Show("Todas posições já estão preenchidas! ", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+                        return;
+                    }
+
+                }
+            }
+
+            if (cbHash.SelectedIndex == 1)
+            {
+                metodoFolding();
+            }
+
+            if (cbHash.SelectedIndex == 2)
+            {
+                metodoMidSquare();
+            }
+
+        }
+
+        public void testaColisao(int resultado)
+        {
+            if (dataGridHash.Rows[resultado].Cells[1].Value == null)
+            {
+                dataGridHash.Rows[resultado].Cells[1].Value = tbValor.Text;
+            }
+            else
+            {
+                int pontoDePartida = resultado;
+
+                for (int i = pontoDePartida + 1; i < dataGridHash.RowCount;)
+                {
+                    if (dataGridHash.Rows[i].Cells[1].Value == null)
+                    {
+                        dataGridHash.Rows[i].Cells[1].Value = tbValor.Text;
+                        return;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                    if (i == 21)
+                    {
+                        for (i = 0; i < pontoDePartida; i++)
+                        {
+                            if (dataGridHash.Rows[i].Cells[1].Value == null)
+                            {
+                                dataGridHash.Rows[i].Cells[1].Value = tbValor.Text;
+                                return;
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void cbHash_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridHash.RowCount; i++)
+            {
+                dataGridHash.Rows[i].DataGridView.Columns.Clear();
+            }
+
+            dataGridHash.ColumnCount = 2;
+            dataGridHash.RowCount = 21;
+            dataGridHash.Columns[0].Width = 50;
+            dataGridHash.Columns[1].Width = 130;
+
+            dataGridHash.Columns[0].Name = "Posição";
+            dataGridHash.Columns[1].Name = "Matricula";
+
+            for (int i = 0; i < dataGridHash.RowCount; i++)
+            {
+                dataGridHash.Rows[i].Cells[0].Value = i;
+            }
+        }
+
+        private void tbValor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
